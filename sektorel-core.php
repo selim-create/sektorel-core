2da66b3d2407e3e95d75fbe681f40ab98bba339a
Selim Altınkaynak <selim@hipmedya.com>
2026-08-03 18:50:21 +0300

    Initialize company profile API and bump version

--- sektorel-core.php
+++ sektorel-core.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Sektorel Core
  * Description: Sektörel Ajanda projesi için CPT, Taxonomy ve API tanımlarını içeren çekirdek eklenti.
- * Version: 1.16.0
+ * Version: 1.17.0
  * Author: Sektörel Ajanda Dev Team
  * Text Domain: sektorel-core
  */
@@ -38,6 +38,7 @@ class Sektorel_Core {
         }
 
         Sektorel_Company_Mutations::init();
+        Sektorel_Company_Profile::init();
         Sektorel_Company_Settings::init();
         Sektorel_Company_Members::init();
         Sektorel_Auth_Mutations::init();
@@ -73,6 +74,7 @@ class Sektorel_Core {
         require_once SEKTOREL_CORE_PATH . 'includes/graphql/class-auth-mutations.php';
         require_once SEKTOREL_CORE_PATH . 'includes/graphql/class-password-reset-mutations.php';
         require_once SEKTOREL_CORE_PATH . 'includes/graphql/class-company-mutations.php';
+        require_once SEKTOREL_CORE_PATH . 'includes/graphql/class-company-profile.php';
         require_once SEKTOREL_CORE_PATH . 'includes/graphql/class-company-settings.php';
         require_once SEKTOREL_CORE_PATH . 'includes/graphql/class-company-members.php';
         require_once SEKTOREL_CORE_PATH . 'includes/graphql/class-session-query.php';
