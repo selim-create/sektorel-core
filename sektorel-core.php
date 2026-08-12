3ef51a9943692a5eac5a203a3f6e39f0615d4307
Selim Altınkaynak <selim@hipmedya.com>
2026-08-12 08:17:07 +0300

    Core 1.36.6: bump version for mapping page load hotfix

--- sektorel-core.php
+++ sektorel-core.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Sektorel Core
  * Description: Sektörel Ajanda projesi için CPT, Taxonomy ve API tanımlarını içeren çekirdek eklenti.
- * Version: 1.36.5
+ * Version: 1.36.6
  * Author: Sektörel Ajanda Dev Team
  * Text Domain: sektorel-core
  */
