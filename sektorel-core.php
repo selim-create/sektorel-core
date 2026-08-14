b6beb87a5880df93213fa33ec4bfe01983104ea8
Selim Altınkaynak <selim@hipmedya.com>
2026-08-14 22:13:35 +0300

    Core 1.48.1: bump plugin version

--- sektorel-core.php
+++ sektorel-core.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Sektorel Core
  * Description: Sektörel Ajanda projesi için CPT, Taxonomy ve API tanımlarını içeren çekirdek eklenti.
- * Version: 1.48.0
+ * Version: 1.48.1
  * Author: Sektörel Ajanda Dev Team
  * Text Domain: sektorel-core
  */
